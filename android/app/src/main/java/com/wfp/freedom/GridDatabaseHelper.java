6247d96df671ffa37a622507ae0b69b197aa7f1a
wang-feiping <57311451+wang-feiping@users.noreply.github.com>
2020-08-17 20:39:54 +0800

    Update GridDatabaseHelper.java

--- android/app/src/main/java/com/wfp/freedom/GridDatabaseHelper.java
+++ android/app/src/main/java/com/wfp/freedom/GridDatabaseHelper.java
@@ -1,58 +1,59 @@
 package com.wfp.freedom;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
+import android.content.ContentValues;
 
-import androidx.annotation.Nullable;
+import com.wfp.freedom.slide.SlideData;
 
-public class GridDatabaseHelper extends SQLiteOpenHelper {
-    private static final String TAG = "Freedom";
+import java.util.ArrayList;
+import java.util.List;
 
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "grid.db";
+public class DataHelper {
 
-    public GridDatabaseHelper(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    public List<ContentValues> initialPlanData(String code) {
+        List<ContentValues> datas = new ArrayList<>();
+
+        // 改为从配置文件中读取
+        ContentValues values1 = new ContentValues();
+        values1.put("code", "512800");
+        values1.put("id", 1);
+        values1.put("buyPrice", 1.23);
+        values1.put("sellPrice", 1.29);
+        values1.put("buyCount", 1700);
+        values1.put("sellCount", 1600);
+        values1.put("profit", 104.55);
+        values1.put("profitRation", 0.05);
+        values1.put("saveCount", 100);
+        datas.add(values1);
+
+        ContentValues values2 = new ContentValues();
+        values2.put("code", "512800");
+        values2.put("id", 2);
+        values2.put("buyPrice", 1.17);
+        values2.put("buyCount", 1800);
+        values2.put("sellPrice", 1.23);
+        values2.put("sellCount", 1700);
+        values2.put("profit", 105.17);
+        values2.put("profitRation", 0.05);
+        values2.put("saveCount", 100);
+        datas.add(values2);
+        return datas;
+
+       /* SlideData data = new SlideData();
+        List<String> title = new ArrayList<>();
+        title.add("序号");
+        title.add("买入价格");
+        title.add("卖出价格");
+        title.add("买入数量");
+        title.add("买入金额");
+        title.add("卖出数量");
+        title.add("卖出金额");
+        title.add("盈利金额");
+        title.add("盈利比例");
+        title.add("留存数量");*/
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        Log.i(TAG, "GridDatabaseHelper onCreate");
-
-        String createPlanTable = "create table plan(" +
-                "code char(10) NOT NULL," +
-                "id integer  NOT NULL," +
-                "buyPrice decimal(8,2) NOT NULL," +
-                "buyCount integer NOT NULL," +
-                "sellPrice decimal(8,2) NOT NULL," +
-                "sellCount integer NOT NULL," +
-                "profit decimal(8,2) NOT NULL," +
-                "profitRation decimal(8,2) NOT NULL," +
-                "saveCount integer NOT NULL," +
-                "PRIMARY KEY (code, id))";
-
-        db.execSQL(createPlanTable);
-
-        String createRecordTable = "create table record(" +
-                "code char(10) NOT NULL," +
-                "id integer NOT NULL," +
-                "date char(20) NOT NULL," +
-                "operation integer NOT NULL," +
-                "price decimal(8,2) NOT NULL," +
-                "count integer NOT NULL," +
-                "currentHold integer NOT NULL," +
-                "currentValue decimal(8,2) NOT NULL," +
-                "costPrice decimal(8,2) NOT NULL," +
-                "currentPrice decimal(8,2) NOT NULL," +
-                "PRIMARY KEY (code, id))";
-
-        db.execSQL(createRecordTable);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.i(TAG, "GridDatabaseHelper onUpgrade");
+    // 获取指定标的的网格计划表
+    public SlideData getPlanData(String code) {
+        return null;
     }
 }
