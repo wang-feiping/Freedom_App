6d3a1682cfb7805f65a4d9033a0090160e480fd5
wang-feiping <57311451+wang-feiping@users.noreply.github.com>
2020-08-18 09:54:24 +0800

    Update PlanActivity.java

--- android/app/src/main/java/com/wfp/freedom/PlanActivity.java
+++ android/app/src/main/java/com/wfp/freedom/PlanActivity.java
@@ -2,19 +2,21 @@ package com.wfp.freedom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.wfp.freedom.slide.SlideData;
 import com.wfp.freedom.slide.SlideWrap;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PlanActivity extends AppCompatActivity {
-    public static final String TAG = "Freedom";
+    private static final String TAG = "Freedom";
+
+    private DataHelper dataHelper = new DataHelper();
 
     @BindView(R.id.draggable_list_view)
     public SlideWrap mDraggableView;
@@ -29,53 +31,12 @@ public class PlanActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        mDraggableView.setAdapter(new ColumnAdapter(this, obtainDraggableData()));
-    }
+        GridDatabaseHelper databaseHelper = new GridDatabaseHelper(this);
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
 
-    private SlideData obtainDraggableData() {
-        SlideData data = new SlideData();
-        List<String> title = new ArrayList<>();
-        title.add("序号");
-        title.add("买入价格");
-        title.add("卖出价格");
-        title.add("买入数量");
-        title.add("买入金额");
-        title.add("卖出数量");
-        title.add("卖出金额");
-        title.add("盈利金额");
-        title.add("盈利比例");
-        title.add("留存数量");
-
-        List<String> column1 = new ArrayList<>();
-        column1.add("1");
-        column1.add("1.23");
-        column1.add("1.29");
-        column1.add("1700");
-        column1.add("2091.00");
-        column1.add("1600");
-        column1.add("2066.40");
-        column1.add("104.55");
-        column1.add("5.00%");
-        column1.add("100");
-
-        List<String> column2 = new ArrayList<>();
-        column2.add("2");
-        column2.add("1.17");
-        column2.add("1.23");
-        column2.add("1800");
-        column2.add("2103.00");
-        column2.add("1700");
-        column2.add("2085.77");
-        column2.add("105.17");
-        column2.add("5.00%");
-        column2.add("100");
-
-        List<List<String>> content = new ArrayList<>();
-        content.add(column1);
-        content.add(column2);
-
-        data.setTitle(title);
-        data.setContent(content);
-        return data;
+        String queryPlan = "SELECT * from plan";
+        Cursor cursor = db.rawQuery(queryPlan, null);
+        SlideData slideData = dataHelper.getPlanData(cursor);
+        mDraggableView.setAdapter(new ColumnAdapter(this, slideData));
     }
 }
