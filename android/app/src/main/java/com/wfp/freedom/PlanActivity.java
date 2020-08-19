0be680cdbc651772fd7e7556cdc91282bc8a1ead
wang-feiping <57311451+wang-feiping@users.noreply.github.com>
2020-08-19 17:21:55 +0800

    Update PlanActivity.java

--- android/app/src/main/java/com/wfp/freedom/PlanActivity.java
+++ android/app/src/main/java/com/wfp/freedom/PlanActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
 import com.wfp.freedom.slide.SlideData;
 import com.wfp.freedom.slide.SlideWrap;
@@ -18,6 +18,9 @@ public class PlanActivity extends AppCompatActivity {
 
     private DataHelper dataHelper = new DataHelper();
 
+    @BindView(R.id.title)
+    public TextView mTitleView;
+
     @BindView(R.id.draggable_list_view)
     public SlideWrap mDraggableView;
 
@@ -27,14 +30,15 @@ public class PlanActivity extends AppCompatActivity {
         setContentView(R.layout.activity_plan);
         ButterKnife.bind(this);
 
-        initData();
+        mTitleView.setText("证券ETF网格");
+        initData("512800");
     }
 
-    private void initData() {
+    private void initData(String code) {
         GridDatabaseHelper databaseHelper = new GridDatabaseHelper(this);
         SQLiteDatabase db = databaseHelper.getWritableDatabase();
 
-        String queryPlan = "SELECT * from plan";
+        String queryPlan = String.format("SELECT * from plan WHERE CODE = %s", code);
         Cursor cursor = db.rawQuery(queryPlan, null);
         SlideData slideData = dataHelper.getPlanData(cursor);
         mDraggableView.setAdapter(new ColumnAdapter(this, slideData));
